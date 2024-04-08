a2e0c021a03e28c54bd904bb964315f962a2bc43
Isadora <106554588+IsadoraAlcantara@users.noreply.github.com>
2024-04-08 18:16:00 -0300

    seção 12 teminada

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,9 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissions",
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
